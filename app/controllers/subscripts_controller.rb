c771bd64314c9fe58b01328d2ed9a04131c8bc4f
Marcus Barros <113722222+Marcus-Barros@users.noreply.github.com>
2022-10-31 16:24:06 +0000

    Start v0.2 Release

---
+++ app/controllers/subscripts_controller.rb
@@ -0,0 +1,2 @@
+class SubscriptsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index', to: 'home#index'
   root 'home#index'
+  resources :subscripts, only:[new]
+  
+
 end
